22029db9079cbf81e4ff29d2d9d4fb266f9e39df
Lashi Thendra <lashithendrabandara@gmail.com>
2023-09-13 21:17:58 +0530

    remove docs and moved to another repo

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,12 +8,19 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
   baseUrl = "assets/";
   projects = [
+    
     {
       title:"POS-SYSTEM",
       url : "pos.png",
       toLink:"https://github.com/lashi-thendra/pos-system",
       discription :` A comprehensive Point of Sale (POS) system that efficiently manages customers and orders. This project utilizes a wide range of technologies on both the front-end and back-end to provide a seamless and user-friendly experience.
       Back end is based on Spring`
+    },
+    {
+      title:"ONLINE IMAGE GALLERY",
+      url : "gal.png",
+      toLink: "https://github.com/lashi-thendra/public-gallery",
+      discription: `This web application allows users to view a gallery of images, upload new images, and download individual images. It is a basic example of how to create an image gallery with Spring and front-end technologies. `
     },
     {
       title:"INFINITY GAME",
@@ -28,13 +35,7 @@ export class ProjectsComponent {
       discription: `A chess game powered by HTML, CSS, JavaScript, and jQuery.
       Features include an AI player with predictive abilities up to a depth of 4. The game is constructed using object-oriented programming with JavaScript.`
     },
-    {
-      title:"BROWSER BASED CHESS ENGINE",
-      url : "chess.png",
-      toLink: "https://lashi-thendra.github.io/webchess/",
-      discription: `A chess game powered by HTML, CSS, JavaScript, and jQuery.
-      Features include an AI player with predictive abilities up to a depth of 4. The game is constructed using object-oriented programming with JavaScript.`
-    }
+    
 
   ];
 }
